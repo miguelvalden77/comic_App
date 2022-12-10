a3980cfe9403b21789a2ea397c493eb0d2ce48db
miguelvalden <miguelvalden77@gmail.com>
2022-12-10 22:59:05 +0100

    home wallpaper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import bg1 from "../assets/wallpaper_1.webp"
 import bg2 from "../assets/wallpaper_2.jpeg"
 import bg3 from "../assets/wallpaper_3.jpg"
+import bg4 from "../assets/wallpaper_4.jpg"
+import bg5 from "../assets/wallpaper_5.jpg"
+import bg6 from "../assets/wallpaper_6.jpg"
+
 // Estilos
 import Carousel from 'react-bootstrap/Carousel'
 import { useMediaQuery } from 'react-responsive'
@@ -8,14 +12,14 @@ import { useMediaQuery } from 'react-responsive'
 
 const Home = ():JSX.Element =>{
 
-  const isBigScreen = useMediaQuery({ query: '(min-width: 960px)' })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 820px)' })
 
     return <main className="home-body">
     <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bg1}
+          src={isBigScreen ? bg1 : bg4}
           alt="First slide"
         />
 
@@ -23,7 +27,7 @@ const Home = ():JSX.Element =>{
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bg2}
+          src={isBigScreen ? bg2 : bg5}
           alt="Second slide"
         />
 
@@ -31,7 +35,7 @@ const Home = ():JSX.Element =>{
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={bg3}
+          src={isBigScreen ? bg3 : bg6}
           alt="Third slide"
         />
       </Carousel.Item>
